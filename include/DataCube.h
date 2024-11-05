56652c781d8f98e2315d8723147e2310e410cf93
Zach <tcare50@gmail.com>
2024-11-05 14:13:53 +0000

    Fixed LinScaled miscalc

--- include/DataCube.h
+++ include/DataCube.h
@@ -8,10 +8,10 @@
 class DataCube : public Space {
 public:
 	void Init();
-	void Init(std::string&, int, bool debug);
-	void Load(std::string, int, bool debug);
-	float GetSample(float x, float y, float z);
-	void SetTrilinear(bool);
+	void Init(std::string& filename, int data_start, bool debug);
+	void Load(std::string filename, int data_start, bool debug); //!< Parse an exported MHD file into memory, ready to be sampled. 
+	float GetSample(float x, float y, float z); //!< Returns the value from the cube at x, y, and z. Set interpolation with SetTrilinear().
+	void SetTrilinear(bool); //!< Tells the sampling function to use Trilinear. If set to false, use nearest neighbour. 
 
 	std::vector<float> coords_x;
 	std::vector<float> coords_y;
--- src/Cmem.cpp
+++ src/Cmem.cpp
@@ -91,7 +91,11 @@ float CMEM::LinScaled(
         float f = std::pow(std::cos(theta / 2) + a[5] * std::sin(2 * theta) * (1 - std::exp( - theta)), (p1 * (beta_c[0] + beta_c[1] * std::cos(phi) + beta_c[3] * std::pow(std::sin(phi), 2))));
 
         // what is q?
+<<<<<<< Updated upstream
         float q = p2 * charlie * std::exp(dn * (std::pow(phi_n, a[21])) + p2 * charlie * std::exp(ds * (std::pow(phi_s, a[21]))));
+=======
+        float q = p2 * charlie * std::exp(p3 * dn * (std::pow(phi_n, a[21]))) + p2 * charlie * std::exp(p3 * ds * (std::pow(phi_s, a[21])));
+>>>>>>> Stashed changes
 
         float r = p0 * r0_lin * f + q;
 
